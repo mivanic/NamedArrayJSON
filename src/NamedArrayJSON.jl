421522a39e017d92d4e856d45da756ddd4b0e9a0
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2024-11-25 11:22:39 -0500

    Initial code

--- src/NamedArrayJSON.jl
+++ src/NamedArrayJSON.jl
@@ -1,5 +1,60 @@
 module NamedArrayJSON
 
-# Write your package code here.
+export readJSON, writeJSON
+
+function writeJSON(data, filename)
+
+    # Create a new dictionary where data and set names are separate
+    data_out = Dict()
+    for k ∈ keys(data)
+        if size(data[k]) == ()
+            data_out = merge(data_out, Dict(
+                k => Dict(
+                    "data" => data[k]
+                )
+            ))
+        else
+            data_out = merge(data_out, Dict(
+                k => Dict(
+                    "data" => reshape(data[k], prod(size(data[k]))),
+                    "sets" => names(data[k])
+                )
+            ))
+        end
+
+    end
+
+    # Write it into a JSON file
+    open(filename, "w") do f
+        JSON.print(f, data_out)
+    end
+
+end
+
+function readJSON(filename)
+
+    # Read a JSON file
+    datain = JSON.parsefile(filename)
+
+    # Set up an empty dictionary
+    dataout = Dict()
+
+    # Loop through all keys in the JSON we read in
+    for k ∈ keys(datain)
+        # See if there are sets defined
+        if "sets" ∈ keys(datain[k])
+            # If there is a single set, we need to interpert the JSON set differently
+            if length(datain[k]["sets"]) == 1
+                dataout = merge(dataout, Dict(Symbol(k) => NamedArray(reshape(convert(Vector{Float64}, datain[k]["data"]), map(length, datain[k]["sets"])...), datain[k]["sets"][1])))
+            else
+                println(k)
+                dataout = merge(dataout, Dict(Symbol(k) => NamedArray(reshape(convert(Vector{Float64}, datain[k]["data"]), map(length, datain[k]["sets"])...), datain[k]["sets"])))
+            end
+        else
+            dataout = merge(dataout, Dict(Symbol(k) => datain[k]["data"]))
+        end
+    end
+end
+
 
 end
